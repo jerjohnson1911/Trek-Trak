161b75307b25820d279d4057dc9dddbe30779934
Jeremiah Johnson <jerjohnson1911@gmail.com>
2021-03-12 15:25:10 -0700

    post now shows map and centers with marker on post coordinates

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { 
+import {
     GoogleMap,
     useLoadScript,
     Marker,
@@ -16,7 +16,7 @@ const mapContainerStyle = {
 }
 const center = {
     lat: 40.29710871767122,
-    lng:-111.69489911355494
+    lng: -111.69489911355494
 }
 
 const options = {
@@ -24,46 +24,46 @@ const options = {
 }
 
 function Map() {
-const [markers, setMarkers] = useState([])
+    const [markers, setMarkers] = useState([])
 
-console.log(markers)
+    console.log(markers)
 
-useEffect(() => {
-    fetchMarkers()
-}, [])
+    useEffect(() => {
+        fetchMarkers()
+    }, [])
 
-const fetchMarkers = async () => {
-    const res = await axios.get('/api/allCoordinates')
-    setMarkers(res.data)
-}
+    const fetchMarkers = async () => {
+        const res = await axios.get('/api/allCoordinates')
+        setMarkers(res.data)
+    }
 
-const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-})
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    })
 
-if (loadError) return 'Error loading maps!'
-if (!isLoaded) return 'Loading maps!'
+    if (loadError) return 'Error loading maps!'
+    if (!isLoaded) return 'Loading maps!'
 
-return(
-    <div>
-        <GoogleMap 
-        mapContainerStyle={mapContainerStyle} 
-        zoom={8} 
-        center={center}
-        options={options}
+    return (
+        <div>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+                options={options}
 
-        >
-            {markers.map((marker) => (
-            <Marker 
-            key={marker.id}
-            position={{lat: +marker.latitude, lng: +marker.longitude}} 
-            />
-            ))}
-        </GoogleMap>
-        
-    </div>
-)
+            >
+                {markers.map((marker) => (
+                    <Marker
+                        key={marker.id}
+                        position={{ lat: +marker.latitude, lng: +marker.longitude }}
+                    />
+                ))}
+            </GoogleMap>
+
+        </div>
+    )
 
 }
 
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,13 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow
+} from '@react-google-maps/api'
+
+const libraries = ['places']
+const mapContainerStyle = {
+    width: '50vw',
+    height: '50vh'
+}
+const options = {
+    // styles: mapStyles,
+}
+
 
 function Post() {
     // console.log('hit')
     const [post, setPost] = useState([])
-    const {id} = useParams()
+    const { id } = useParams()
+    const center = {
+        lat: +post.latitude,
+        lng: +post.longitude
+    }
 
-    
     useEffect(() => {
         fetchData()
     }, [])
@@ -16,13 +35,50 @@ function Post() {
         // console.log(props)
         const res = await axios.get(`/api/post/${id}`)
         setPost(res.data)
-        
+
     }
+
+    const [markers, setMarkers] = useState([])
+
+    console.log(markers)
+
+    useEffect(() => {
+        fetchMarkers()
+    }, [])
+
+    const fetchMarkers = async () => {
+        const res = await axios.get('/api/allCoordinates')
+        setMarkers(res.data)
+    }
+
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    })
+
+    if (loadError) return 'Error loading maps!'
+    if (!isLoaded) return 'Loading maps!'
 
     return (
         <div>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
+            <div>
+                <GoogleMap
+                    mapContainerStyle={mapContainerStyle}
+                    zoom={15}
+                    center={center}
+                    options={options}
+
+                >
+
+                    <Marker
+                        position={{ lat: +post.latitude, lng: +post.longitude }}
+                    />
+
+                </GoogleMap>
+
+            </div>
         </div>
     )
 
